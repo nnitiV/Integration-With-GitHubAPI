b3d7a4aa3569f28f9599fedf88ed446770807987
Vitor Augusto Elias Cunha <vihstudent@gmail.com>
2025-03-29 11:32:11 -0300

    Made it prettier and a better result to return

--- src/main/java/com/github/api/githubapi/controllers/GitHubController.java
+++ src/main/java/com/github/api/githubapi/controllers/GitHubController.java
@@ -1,7 +1,7 @@
 package com.github.api.githubapi.controllers;
 
-import java.io.File;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +20,7 @@ public class GitHubController {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	@RequestMapping("/")
-	public String home() {
-		return "home";
-	}
-
-	@RequestMapping("/test")
+	@RequestMapping("/get_action")
 	public String test(@RequestParam("username") String username) {
 		String url = "https://api.github.com/users/" + username + "/events";
 		RestTemplate restTemplate = new RestTemplate();
@@ -34,59 +29,71 @@ public class GitHubController {
 			List<GitHubResponse> gitHubResponses = objectMapper.readValue(restTemplate.getForObject(url, String.class),
 					new TypeReference<List<GitHubResponse>>() {
 					});
+			AtomicInteger timesOfCreateEvent = new AtomicInteger(0);
+			AtomicInteger timesOfPushEvent = new AtomicInteger(0);
 			StringBuilder stringToPrint = new StringBuilder();
-			gitHubResponses.stream().forEach(response -> {
-				stringToPrint.append("Action id: " + response.getId());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Type: " + response.getType());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Actor: [");
-				stringToPrint.append("<br>");
-				stringToPrint.append("id: " + response.getActor().getId());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Username: " + response.getActor().getUsername());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Display login: " + response.getActor().getDisplayLogin());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Gravatar id: " + response.getActor().getGravatar_id());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Url: " + response.getActor().getUrl());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Avatar url: " + response.getActor().getAvatarUrl());
-				stringToPrint.append("<br>");
-				stringToPrint.append("]");
-				stringToPrint.append("<br>");
-				stringToPrint.append("Repo: [");
-				stringToPrint.append("<br>");
-				stringToPrint.append("id: " + response.getRepo().getId());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Name: " + response.getRepo().getName());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Url: " + response.getRepo().getUrl());
-				stringToPrint.append("<br>");
-				stringToPrint.append("]");
-				stringToPrint.append("<br>");
-				stringToPrint.append("Payload: [");
-				stringToPrint.append("<br>");
-				stringToPrint.append("Ref: " + response.getPayload().getRef());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Ref type: " + response.getPayload().getRefType());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Get master branch: " + response.getPayload().getMasterBranch());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Get description: " + response.getPayload().getDescription());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Get pusher type: " + response.getPayload().getPusherType());
-				stringToPrint.append("<br>");
-				stringToPrint.append("]");
-				stringToPrint.append("<br>");
-				stringToPrint.append("Is public? " + response.isPublic());
-				stringToPrint.append("<br>");
-				stringToPrint.append("Created at: " + response.getData());
-				stringToPrint.append("<br>");
-				stringToPrint.append("============================");
-				stringToPrint.append("<br>");
+
+			gitHubResponses.forEach(response -> {
+			    switch(response.getType().toLowerCase()) {
+			        case "createevent" -> timesOfCreateEvent.incrementAndGet();
+			        case "pushevent" -> timesOfPushEvent.incrementAndGet();
+			    }
 			});
+			stringToPrint.append("- Pushed " + timesOfPushEvent + " commits.");
+			stringToPrint.append("<br>");
+			stringToPrint.append("- Created " + timesOfCreateEvent + " times.");
+//			gitHubResponses.stream().forEach(response -> {
+//				stringToPrint.append("Action id: " + response.getId());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Type: " + response.getType());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Actor: [");
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("id: " + response.getActor().getId());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Username: " + response.getActor().getUsername());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Display login: " + response.getActor().getDisplayLogin());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Gravatar id: " + response.getActor().getGravatar_id());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Url: " + response.getActor().getUrl());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Avatar url: " + response.getActor().getAvatarUrl());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("]");
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Repo: [");
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("id: " + response.getRepo().getId());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Name: " + response.getRepo().getName());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Url: " + response.getRepo().getUrl());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("]");
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Payload: [");
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Ref: " + response.getPayload().getRef());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Ref type: " + response.getPayload().getRefType());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Get master branch: " + response.getPayload().getMasterBranch());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Get description: " + response.getPayload().getDescription());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Get pusher type: " + response.getPayload().getPusherType());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("]");
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Is public? " + response.isPublic());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("Created at: " + response.getData());
+//				stringToPrint.append("<br>");
+//				stringToPrint.append("============================");
+//				stringToPrint.append("<br>");
+//			});
 			return stringToPrint.toString();
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
